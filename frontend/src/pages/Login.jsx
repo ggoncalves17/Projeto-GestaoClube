b05ac15549c2524a0df7b23e6168fbaedb39c09c
Guilherme Gonçalves <guigonzo.1@hotmail.com>
2025-03-15 15:57:12 +0000

    Frontend - Login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import LogoCoja from "../assets/LogoCoja.png";
 import "./Login.css";
+import axios from 'axios';
 
 const Login = () => {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const autenticaUtilizador = (event) => {
+    event.preventDefault();
+
+    axios.post("http://localhost:8000/api/login/", {
+      email: email,
+      password: password,
+    })
+    .then(res => {
+      console.log("Resposta do Backend: ", res.data)
+    })
+    .catch(err => {
+        console.log("Código do erro:", err.response.status); 
+        console.log("Mensagem do erro:", err.response.data.message); 
+    })
+  }
+
   return (
-    <div id="divteste">
+    <div id="divteste">      
       <div id="box-Login">
         <div id="imagem-Login">
           <img id="logo-Login" src={LogoCoja} alt="LogoClube" />
@@ -13,10 +34,10 @@ const Login = () => {
           <h2>Bem Vindo!</h2>
           <h2>Iniciar Sessão</h2>
 
-          <form>
-            <input name="email" placeholder="Email" />
-            <input type="email" name="password" placeholder="Password" />
-            <button>Entrar</button>
+          <form onSubmit={autenticaUtilizador}>
+            <input onChange={(e) => setEmail(e.target.value)} type="email" name="email" placeholder="Email" value={email} required />
+            <input onChange={(e) => setPassword(e.target.value)} name="password" placeholder="Password" value={password} required/>
+            <button type="submit">Entrar</button>
           </form>
 
           <a id="corAzul-Login" href="#">
