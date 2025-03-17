6d40efa731095bc8cc976bc395f7b68619f63bcb
Guilherme Gonçalves <guigonzo.1@hotmail.com>
2025-03-17 17:12:53 +0000

    Começo da Sidebar

--- frontend/src/Layouts/MainLayout.jsx
+++ frontend/src/Layouts/MainLayout.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
-import Sidebar from '../components/Sidebar';
+import Sidebar from '../components/Sidebar/Sidebar';
 import "./MainLayout.css"
 
 const MainLayout = () => {
---
+++ frontend/src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import styles from "./Sidebar.module.css";
+import Calendario from "../../assets/Icons-Sidebar/calendario.png";
+import Dashboard from "../../assets/Icons-Sidebar/dashboard.png";
+import Utilizadores from "../../assets/Icons-Sidebar/utilizadores.png";
+import Financas from "../../assets/Icons-Sidebar/finanças.png";
+import Evento from "../../assets/Icons-Sidebar/evento.png";
+import Clube from "../../assets/Icons-Sidebar/clube.png";
+import { FaBars } from "react-icons/fa";
+
+const Sidebar = () => {
+  const [isExpandido, setIsExpandido] = useState(true);
+
+  return (
+    <div className={`${styles.menu} ${isExpandido ? styles.expandido : styles.contraido}`}>
+      <button className={styles.botaoAbreFecha} onClick={() => setIsExpandido(!isExpandido)}>
+        <FaBars className={styles.iconBotao}/>
+      </button>
+      <ul>
+        <li>
+          <img className={styles.icon} src={Dashboard} alt="IconDashboard" />
+          {!isExpandido ? "" : "Dashboard"}
+        </li>
+        <li>
+          <img className={styles.icon} src={Utilizadores} alt="IconUtilizadores" />
+          {!isExpandido ? "" : "Utilizadores"}
+        </li>
+        <li>
+          <img className={styles.icon} src={Clube} alt="IconClube" />
+          {!isExpandido ? "" : "Clube"}
+        </li>
+        <li>
+          <img className={styles.icon} src={Financas} alt="IconFinancas" />
+          {!isExpandido ? "" : "Finanças"}
+        </li>
+        <li>
+          <img className={styles.icon} src={Evento} alt="IconEvento" />
+          {!isExpandido ? "" : "Evento"}
+        </li>
+        <hr />
+        <li>
+          <img className={styles.icon} src={Calendario} alt="IconCalendario" />
+          {!isExpandido ? "" : "Calendário"}
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Sidebar;
